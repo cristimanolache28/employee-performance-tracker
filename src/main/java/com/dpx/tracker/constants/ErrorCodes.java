746a2a962b9130223c2240e3dbb9658cea8b06c4
Cristian Manolache <cristianmanolache0@gmail.com>
2025-06-26 15:40:09 +0300

    Implement Exception Hierarchy Pattern for custom exceptions

---
+++ src/main/java/com/dpx/tracker/constants/ErrorCodes.java
@@ -0,0 +1,11 @@
+package com.dpx.tracker.constants;
+
+public final class ErrorCodes {
+
+    private ErrorCodes() {
+        throw new UnsupportedOperationException("Utility class, do not instantiate");
+    }
+
+    public static final String ROLE_NOT_FOUND = "ROLE_NOT_FOUND";
+
+}
--- src/main/java/com/dpx/tracker/exception/ResourceNotFoundException.java
+++ src/main/java/com/dpx/tracker/exception/ResourceNotFoundException.java
@@ -1,24 +1,9 @@
 package com.dpx.tracker.exception;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor(force = true)
-@Data
-public class ResourceNotFoundException extends RuntimeException {
-    private  String errorCode;
-    private  HttpStatus status;
-
-    public ResourceNotFoundException(String errorMessage, String errorCode, HttpStatus status) {
-        super(errorMessage);
-        this.errorCode = errorCode;
-        this.status = status;
-    }
-
-    public ResourceNotFoundException(String message) {
-        super(message);
+public class ResourceNotFoundException extends BaseException {
+    public ResourceNotFoundException(String message, String errorCode) {
+        super(message, errorCode, HttpStatus.NOT_FOUND);
     }
 }
