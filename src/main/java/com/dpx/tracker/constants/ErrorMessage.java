111a52bf8bd4d205016a0bfed0bd25cc4eac5964
Cristian Manolache <cristianmanolache0@gmail.com>
2025-07-09 20:53:55 +0300

    add Layered Architecture for Position entity

--- src/main/java/com/dpx/tracker/constants/ErrorMessage.java
+++ src/main/java/com/dpx/tracker/constants/ErrorMessage.java
@@ -8,5 +8,7 @@ public final class ErrorMessage {
 
     public static final String ROLE_NOT_FOUND = "Role with id %s not found";
     public static final String ROLE_LIST_EMPTY = "Roles list is empty.";
-    public static final String ROLE_CREATE_DTO_NULL = "RoleCreateDto cannot be null";
+    public static final String ROLE_CREATE_DTO_NULL = "RoleCreateDto can not be null";
+
+    public static final String POSITION_CREATE_DTO_NULL = "PositionCreateDto can not be null";
 }
