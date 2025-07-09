51bb4baf643538f517da8cb810720a78ac45eaac
Cristian Manolache <cristianmanolache0@gmail.com>
2025-07-09 20:33:15 +0300

    Add CreatePosition and ResponsePosition DTOs and their mapper for Position entity

--- src/main/java/com/dpx/tracker/controller/RoleController.java
+++ src/main/java/com/dpx/tracker/controller/RoleController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
