9e4f1d22cfd13b05d2ee8807d4e3a93f865e614f
Cristian Manolache <cristianmanolache0@gmail.com>
2025-07-13 20:10:50 +0300

    Create controller and endpoints for all SkillLevelStage CRUD operations

--- src/main/java/com/dpx/tracker/constants/EndpointConstants.java
+++ src/main/java/com/dpx/tracker/constants/EndpointConstants.java
@@ -7,5 +7,6 @@ public final class EndpointConstants {
     }
 
     public static final String ROLE_ENDPOINT = "/api/v1/roles";
+    public static final String SKILL_LEVEL_STAGE_ENDPOINT = "/api/v1/skill-level-stage";
 
 }
--- src/main/java/com/dpx/tracker/service/SkillLevelStageService.java
+++ src/main/java/com/dpx/tracker/service/SkillLevelStageService.java
@@ -1,12 +1,10 @@
 package com.dpx.tracker.service;
 
-import com.dpx.tracker.dto.skilllevelstages.DeleteSkillLevelStageResponse;
+import com.dpx.tracker.dto.skilllevelstages.SkillLevelStageDeleteResponse;
 import com.dpx.tracker.dto.skilllevelstages.SkillLevelStageCreateDto;
 import com.dpx.tracker.dto.skilllevelstages.SkillLevelStageResponseDto;
-import com.dpx.tracker.entity.Skill;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 public interface SkillLevelStageService {
@@ -15,7 +13,7 @@ public interface SkillLevelStageService {
 
     SkillLevelStageResponseDto getSkillLevelStageById(UUID id);
 
-    DeleteSkillLevelStageResponse deleteSkillLevelStageById(UUID id);
+    SkillLevelStageDeleteResponse deleteSkillLevelStageById(UUID id);
 
     List<SkillLevelStageResponseDto> getAllSkillLevelStages();
 
